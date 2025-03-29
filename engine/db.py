cc8aa6d623cfe13e9dc428296c2c83bfadfa73f7
Vanshika Garg <vanshikagarg59@gmail.com>
2025-03-29 17:24:37 +0530

    apps and links update

--- engine/db.py
+++ engine/db.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query) 
 
-# query = "INSERT INTO sys_command VALUES(null,'ONENOTE','C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
+# query = "INSERT INTO sys_command VALUES(null,'Microsoft Edge','C:\Program Files (x86)\Microsoft\Edge\Application\\msedge.exe')"
 # cursor.execute(query)
 # conn.commit()
 # cursor.execute("DELETE FROM sys_command WHERE id = 2")
@@ -16,6 +16,6 @@ cursor.execute(query)
 query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
 cursor.execute(query)
 
-# query = "INSERT INTO web_command VALUES (null,'YouTube', 'https://www.youtube.com/')"
+# query = "INSERT INTO web_command VALUES (null,'Gmail', 'https://mail.google.com/mail/u/0/#inbox')"
 # cursor.execute(query)
 # conn.commit()
